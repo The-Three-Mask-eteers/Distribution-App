659bb86a1955fecdbe754f1ef61816a8c5c2c226
Tuffy101 <69167812+Tuffy101@users.noreply.github.com>
2020-08-21 11:12:26 -0700

    I added a hashing function.

--- app/src/main/java/com/example/loginapp/AES.java
+++ app/src/main/java/com/example/loginapp/AES.java
@@ -4,6 +4,8 @@ import android.os.Build;
 import androidx.annotation.RequiresApi;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -67,5 +69,24 @@ public class AES {
             System.out.println("Error while decrypting: " + e.toString());
         }
         return null;
+    }
+
+    public static String md5(String s)
+    {
+        MessageDigest digest;
+        try
+        {
+            digest = MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes(Charset.forName("US-ASCII")),0,s.length());
+            byte[] magnitude = digest.digest();
+            BigInteger bi = new BigInteger(1, magnitude);
+            String hash = String.format("%0" + (magnitude.length << 1) + "x", bi);
+            return hash;
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
