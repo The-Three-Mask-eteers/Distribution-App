ab0e1f608cd144eccb8ecfa2fa97a70695c30df8
Sidhya6 <69813729+Sidhya6@users.noreply.github.com>
2020-08-29 14:02:15 -0700

    Latest changes, please test on an android.

--- app/src/main/java/com/example/loginapp/HomeActivity.java
+++ app/src/main/java/com/example/loginapp/HomeActivity.java
@@ -1,15 +1,22 @@
 package com.example.loginapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
-
-
-
     private Button button;
     private Button button2;
 
@@ -20,30 +27,28 @@ public class HomeActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.button);
         button2 = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
 
+
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 openDonateAMask();
 
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View view) {
                 openCollectAMask();
 
             }
         });
     }
-    public void openDonateAMask() {
-//        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-//        Donatee.donateeString = Donatee.donatee1;
-//        startActivity(intent);
-    }
-    public void openCollectAMask() {
-//        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-//
-//        startActivity(intent);
-    }
-
+        public void openDonateAMask() {
+            Intent intent = new Intent(this, DonateAMask.class);
+            startActivity(intent);
+        }
+        public void openCollectAMask() {
+            Intent intent = new Intent( this, CollectAMask.class);
+            startActivity(intent);
+        }
 }
+
