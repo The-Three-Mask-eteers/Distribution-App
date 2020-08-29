5db06e6c10934c7b473a34ef4b28a58680e2cc1e
Sidhya6 <69813729+Sidhya6@users.noreply.github.com>
2020-08-28 21:25:05 -0700

    I linked the buttons to different pages + new changes

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -6,45 +6,74 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.viewPager);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDonateAMask();
 
-        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
-        pagerAdapter.addFragmet(new LoginFragment());
-        pagerAdapter.addFragmet(new RegisterFragment());
-        viewPager.setAdapter(pagerAdapter);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCollectAMask();
+
+            }
+        });
+
+
+                ViewPager viewPager = findViewById(R.id.viewPager);
+
+                AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+                pagerAdapter.addFragmet(new LoginFragment());
+                pagerAdapter.addFragmet(new RegisterFragment());
+                viewPager.setAdapter(pagerAdapter);
+            }
+
+    private void openCollectAMask() {
+    }
+
+    private void openDonateAMask() {
     }
 
     class AuthenticationPagerAdapter extends FragmentPagerAdapter {
-        private ArrayList<Fragment> fragmentList = new ArrayList<>();
+                private ArrayList<Fragment> fragmentList = new ArrayList<>();
 
-        public AuthenticationPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+                public AuthenticationPagerAdapter(FragmentManager fm) {
+                    super(fm);
+                }
 
-        @Override
-        public Fragment getItem(int i) {
-            return fragmentList.get(i);
-        }
+                @Override
+                public Fragment getItem(int i) {
+                    return fragmentList.get(i);
+                }
 
-        @Override
-        public int getCount() {
-            return fragmentList.size();
-        }
+                @Override
+                public int getCount() {
+                    return fragmentList.size();
+                }
         /*String hello = "hello";
        String hello2 = Enc*/
 
-        void addFragmet(Fragment fragment) {
-            fragmentList.add(fragment);
+                void addFragmet(Fragment fragment) {
+                    fragmentList.add(fragment);
+                }
+            }
         }
-    }
-}
